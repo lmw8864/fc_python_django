e6d46893e97f92a98b601aea555f7cb2d5b39d30
lmw8864 <lmw8864@gmail.com>
2017-10-10 11:02:28 +0900

    fc_first_site: settings.py 설정

--- fc_first_site/fc_first_site/settings.py
+++ fc_first_site/fc_first_site/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'fc_first_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
