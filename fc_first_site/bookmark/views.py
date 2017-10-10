39ddbabb0e3ccf67c9999358105fd54a5530044b
lmw8864 <lmw8864@gmail.com>
2017-10-10 13:57:46 +0900

    fc_first_site: 등록, 수정, 삭제 페이지 구현(각각의 뷰 코딩 및 URL 매핑, 템플릿 코딩)

--- fc_first_site/bookmark/views.py
+++ fc_first_site/bookmark/views.py
@@ -1,11 +1,41 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Bookmark
+from django.core.urlresolvers import reverse_lazy
+
+# 동작하는데 로그인이 필요하도록 하려면 아래 클래스를 상속
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class BookmarkList(ListView):
+class BookmarkList(LoginRequiredMixin, ListView):
     model = Bookmark
 
 
-class BookmarkDetail(DetailView):
+class BookmarkDetail(LoginRequiredMixin, DetailView):
     model = Bookmark
+
+
+class BookmarkCreate(LoginRequiredMixin, CreateView):
+    model = Bookmark
+    fields = ['title', 'url']
+    success_url = reverse_lazy('bookmark:index')
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+
+        if form.is_valid:
+            form.instance.save()
+            return redirect('/bookmark/')
+        else:
+            return self.render_to_response(({'form': form}))
+
+
+class BookmarkUpdate(LoginRequiredMixin, UpdateView):
+    model = Bookmark
+    fields = ['title', 'url']
+    success_url = reverse_lazy('bookmark:index')
+
+
+class BookmarkDelete(LoginRequiredMixin, DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('bookmark:index')
