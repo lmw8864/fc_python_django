5319217260018a4e268a1c305a408512aa544114
lmw8864 <lmw8864@gmail.com>
2017-11-02 08:40:00 +0900

    fc_first_site: photo 앱 모델 구현 및 admin 등록

--- fc_first_site/photo/models.py
+++ fc_first_site/photo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Photo(models.Model):
+    author = models.ForeignKey(User, related_name='photo_posts')
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-updated',)
+
+    def __str__(self):
+        return self.author.username + " " + self.created.strftime("%Y-%m-%d %H:%M:%S")
