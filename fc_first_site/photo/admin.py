f2040e43171c10ce574fb627980f5937dc14b257
lmw8864 <lmw8864@gmail.com>
2017-11-02 08:51:26 +0900

    fc_first_site: 관리자 항목 커스터마이징

--- fc_first_site/photo/admin.py
+++ fc_first_site/photo/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Photo
 
-admin.site.register(Photo)
+
+class PhotoAmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'created', 'updated')
+    list_filter = ('author', 'created', 'updated')
+    search_fields = ('author', 'text', 'created')
+    raw_id_fields = ('author',)
+    ordering = ['updated', 'created']
+
+
+admin.site.register(Photo, PhotoAmin)
