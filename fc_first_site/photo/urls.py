462eb3593080df7716ad789ad9cdec0d24330917
lmw8864 <lmw8864@gmail.com>
2017-11-06 12:05:58 +0900

    fc_first_site: photo 앱 Detail 페이지 구현

--- fc_first_site/photo/urls.py
+++ fc_first_site/photo/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.views.generic import DetailView
+from .models import Photo
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^single/(?P<pk>\d+)/$', DetailView.as_view(model=Photo, template_name='photo/detail.html'), name='post_detail'),
 ]
