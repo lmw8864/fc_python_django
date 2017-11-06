eb82a0e43c9f5107dbda5d7a8a6f36e7815f4be5
lmw8864 <lmw8864@gmail.com>
2017-11-06 10:40:43 +0900

    fc_first_site: Decorator 추가(@login_required)

--- fc_first_site/photo/views.py
+++ fc_first_site/photo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required  # for 함수형 뷰
 from .models import Photo
 
 
+@login_required
 def post_list(request):
     posts = Photo.objects.all()
     return render(request, 'photo/list.html', {'posts': posts})
