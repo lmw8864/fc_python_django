6ef1d2e6f1584b23c5ede010be37d62933e07eaf
lmw8864 <lmw8864@gmail.com>
2017-11-07 08:46:06 +0900

    fc_first_site: 사이트 가입(Register) 기능 구현

--- fc_first_site/accounts/urls.py
+++ fc_first_site/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
+from .views import *
 
 urlpatterns = [
     # url(r'^', include('django.contrib.auth.urls')),  # 장고 내장 로그인 기능 사용 -> forms, views를 직접 구현할 필요가 없음.
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    url(r'^register/$', register, name='register'),
 ]
